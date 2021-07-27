1e8513618845830a2fcc9e407f34d45de7edcfd9
zoo868e <zoo868e@gmail.com>
2021-07-27 20:44:12 +0800

    The parser of the score_matrix
    Read the score matrix by a parser and parse the score matrix to the
    instance who call the parser.
    It can parse the score matrix now. Maybe implement the version that can
    load the data of DDSresult in the future.

---
+++ Gameinfo/parser.py
@@ -0,0 +1,37 @@
+import sys
+
+def parse_score(row_score):
+    data = row_score.split(";")
+    ret = []
+    one_suit = []
+    for i in range(len(data)):
+        one_suit.clear()
+        score = data[i].split(",")
+        for score_idx in range(len(score)):
+            one_suit.append(float(score[score_idx]))
+        ret.append(one_suit.copy())
+    return ret
+
+def parse_score_matrix(filename):
+    f = open(filename)
+    rowdata = f.read()
+    f.close()
+    data = rowdata.splitlines()
+    ret = []
+    one_matrix = []
+    for i in data:
+        one_matrix.clear()
+        name = i.split(":")[0]
+        score = i.split(":")[1]
+        forNT = score.split("@")[1]
+        forSuit = score.split("@")[0]
+        one_matrix.append(name)
+        one_matrix.append(parse_score(forSuit).copy())
+        one_matrix.append(parse_score(forNT).copy())
+        ret.append(one_matrix.copy())
+    return ret
+
+if __name__ == "__main__":
+    result = parse_score_matrix("data/score_matrix.txt")
+    for i in result:
+        print(i[0])
