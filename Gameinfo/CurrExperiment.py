98080bebbfaec034e4eee61d9649ecdaa762c279
zoo868e <zoo868e@gmail.com>
2021-07-29 19:31:03 +0800

    Add the new experiment
    Add the Longsuit20210729.
    It is similar to the Longsuit20210715, the difference is
    --0715--
    Only add the longgest and also biggest trick to calculate the
    correlation coefficient
    --0729--
    Add every score of different suit that have the longgest length to
    calculate the correlation coefficient

--- Gameinfo/CurrExperiment.py
+++ Gameinfo/CurrExperiment.py
@@ -1,14 +1,21 @@
 import Gameinfo.calculate as Gamefunc
 import Gameinfo.Game as Game
 # calculate the correlation coefficient during Suit contract
-def LongestSuit20210715(board, calculator, func, fname):
+def LongestSuit20210715(board, calculator, func):
+    """
+    This function is doing the experiment that only count the longest suit and
+    the result of DDS is more than 6.
+    If there are two same length only calcutae the suit which have the most
+    win tricks.
+    If the tricks they win still same, then only calculate the biggest suit.
+    """
     DDS = []
     score = []
-    print("LongestSuit20210715")
+    print("{:^20}".format(calculator.HCPname), end = " ")
+ 
     for f in range(len(func)):
         DDS.clear()
         score.clear()
-        print(fname[f])
         tooshort = 0
         l = 0
         correct = 0
@@ -16,12 +23,22 @@ def LongestSuit20210715(board, calculator, func, fname):
         two = 0
         for i in board:
             func[f](i)
-            ns_suit = i.longgest[0][0]
+            ns_maxlong = [i.longgest[0][0][0], i.longgest[0][2][0]]
+            we_maxlong = [i.longgest[1][0][0], i.longgest[1][2][0]]
+            for s in range(len(i.longgest[0][0])):
+                if i.longgest[0][2][s] > ns_maxlong[1]:
+                    ns_maxlong[0] = i.longgest[0][0][s]
+                    ns_maxlong[1] = i.longgest[0][2][s]
+            for s in range(len(i.longgest[1][0])):
+                if i.longgest[1][2][s] > we_maxlong[1]:
+                    we_maxlong[0] = i.longgest[1][0][s]
+                    we_maxlong[1] = i.longgest[1][2][s]
+            ns_suit = ns_maxlong[0]
             ns_len = i.longgest[0][1]
-            ns_ddswin = i.longgest[0][2]
-            we_suit = i.longgest[1][0]
+            ns_ddswin = ns_maxlong[1]
+            we_suit = we_maxlong[0]
             we_len = i.longgest[1][1]
-            we_ddswin = i.longgest[1][2]
+            we_ddswin = we_maxlong[1]
             ns = calculator.ns
             we = calculator.we
             if ns_len < 7 and i.South.hand.distributed[ns_suit] < 6 and i.North.hand.distributed[nw_suit] < 6 or ns_ddswin < 7:
@@ -34,18 +51,75 @@ def LongestSuit20210715(board, calculator, func, fname):
             else:
                 score.append(we[we_suit])
                 DDS.append(we_ddswin)
-        print('{:^.4f}'.format(Gamefunc.getcurr(DDS, score)))
+        print('{:^10.4f}'.format(Gamefunc.getcurr(DDS, score)), end = " ")
+    print("")
 
 
-# calculate the correlation coefficient during NT contract
-def NT20210715(board, calculator, func, fname):
+def LongestSuit20210729(board, calculator, func):
+    """
+    This function is doing the experiment that only count the longest suit and
+    the result of DDS is more than 6.
+    If there are two same length calcutae all of them.
+    """
     DDS = []
     score = []
-    print("NT")
+    print("{:^20}".format(calculator.HCPname), end = " ")
+ 
     for f in range(len(func)):
         DDS.clear()
         score.clear()
-        print(fname[f])
+        tooshort = 0
+        l = 0
+        correct = 0
+        one = 0
+        two = 0
+        for i in board:
+            func[f](i)
+            ns_maxlong = [i.longgest[0][0][0], i.longgest[0][2][0]]
+            we_maxlong = [i.longgest[1][0][0], i.longgest[1][2][0]]
+            for s in range(len(i.longgest[0][0])):
+                if i.longgest[0][2][s] > ns_maxlong[1]:
+                    ns_maxlong[0] = i.longgest[0][0][s]
+                    ns_maxlong[1] = i.longgest[0][2][s]
+            for s in range(len(i.longgest[1][0])):
+                if i.longgest[1][2][s] > we_maxlong[1]:
+                    we_maxlong[0] = i.longgest[1][0][s]
+                    we_maxlong[1] = i.longgest[1][2][s]
+            ns_suit = i.longgest[0][0]
+            ns_len = i.longgest[0][1]
+            ns_ddswin = i.longgest[0][2]
+            we_suit = i.longgest[1][0]
+            we_len = i.longgest[1][1]
+            we_ddswin = i.longgest[1][2]
+            ns = calculator.ns
+            we = calculator.we
+            for s in range(len(ns_suit)):
+                if ns_len < 7 and i.South.hand.distributed[ns_suit[s]] < 6 and i.North.hand.distributed[nw_suit[s]] < 6 or ns_ddswin[s] < 7:
+                    tooshort += 1
+                else:
+                    score.append(ns[ns_suit[s]])
+                    DDS.append(ns_ddswin[s])
+            for s in range(len(we_suit)):
+                if we_len < 7 and i.West.hand.distributed[we_suit[s]] < 6 and i.East.hand.distributed[we_suit[s]] < 6 or we_ddswin[s] < 7:
+                    tooshort += 1
+                else:
+                    score.append(we[we_suit[s]])
+                    DDS.append(we_ddswin[s])
+        print('{:^10.4f}'.format(Gamefunc.getcurr(DDS, score)), end = " ")
+    print("")
+
+
+
+# calculate the correlation coefficient during NT contract
+def NT20210715(board, calculator, func):
+    DDS = []
+    score = []
+    print("{:^10}".format("NT20210715"), end = " ")
+    print("")
+    print("{:^10}".format(calculator.HCPname), end = " ")
+    for f in range(len(func)):
+        DDS.clear()
+        score.clear()
         toosmall = 0
         for i in board:
             func[f](i)
@@ -65,18 +139,18 @@ def NT20210715(board, calculator, func, fname):
             else:
                 score.append(we[we_suit])
                 DDS.append(we_ddswin)
-        print('{:^.4f}'.format(Gamefunc.getcurr(DDS, score)))
+        print('{:^10.4f}'.format(Gamefunc.getcurr(DDS, score)), end = " ")
+    print("")
 
 if __name__=="__main__":
-    f = open("test")
+    import sys
+    sys.path.append("~/bridge/data")
+    f = open("data/test")
     rowdata = f.read()
     f.close()
-    NT_neural = [[0, 3.975, 0.342, 0.340, 0.347, 0.341, 0.356, 0.380, 0.358, 0.496, 0.660, 1.047, 1.676, 2.643],[0, 3.971, 0.327, 0.334, 0.314, 0.332, 0.349, 0.331, 0.361, 0.469, 0.663, 1.032, 1.688, 2.643, 3.971], [0, 3.966, 0.329, 0.328, 0.351, 0.341, 0.339, 0.354, 0.375, 0.461, 0.671, 1.056, 1.675, 2.677], [0, 3.989, 0.342, 0.353, 0.345, 0.344, 0.329, 0.356, 0.400, 0.473, 0.684, 1.030, 1.656, 2.655]]
-    Suit_neural = [[0, 3.982, 1.660, 1.664, 1.669, 1.660, 1.684, 1.680, 1.709, 1.782, 1.921, 2.174, 2.569, 3.207], [0, 3.984, 1.670, 1.667, 1.655, 1.673, 1.685, 1.684, 1.719, 1.791, 1.916, 2.167, 2.569, 3.210, 3.984], [0, 3.973, 1.668, 1.663, 1.669, 1.676, 1.680, 1.687, 1.718, 1.780, 1.918, 2.177, 2.572, 3.220], [0, 3.995, 1.667, 1.660, 1.685, 1.663, 1.688, 1.697, 1.723, 1.783, 1.938, 2.172, 2.565, 3.216]]
-    Neural = [NT_neural, NT_neural]
     board = Game.loadDDSresult(rowdata)
-    calculator = Gamefunc.Calculator(HCPlist=Neural)
-    func = [calculator.HCP, calculator.Long, calculator.Short, calculator.Pavlicek, calculator.Loser]
-    fname = ["HCP", "Long", "Short", "Pavlicek", "Loser"]
-    LongestSuit20210715(board, calculator, func, fname)
-    NT20210715(board, calculator, func, fname)
+    calculator = Gamefunc.Calculator()
+#    func = [calculator.HCP, calculator.Long, calculator.Short, calculator.Pavlicek, calculator.Loser]
+    func = [calculator.HCP]
+    LongestSuit20210715(board, calculator, func)
+    LongestSuit20210729(board, calculator, func)
