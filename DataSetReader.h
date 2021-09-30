437ca5a8014f91dffb59d343b165ca572614a7d1
zoo868e <zoo868e@gmail.com>
2021-09-30 18:57:20 +0800

    Move the function to the specific file
    Move the function to loadfile, split string and ...etc to DataSetReader

---
+++ DataSetReader.h
@@ -0,0 +1,7 @@
+#include"bits/stdc++.h"
+#include"Bridge.h"
+vector<string> split(const string &, const string &);
+int cardtoint(char);
+double correlationCoefficient(vector<double> X, vector<double> Y, int n);
+void loadfile(string filename, vector<Team> &team);
+double getcorr(Experiment expe);
