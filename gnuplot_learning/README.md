9a5ebbb8ee993edcf9d93796eb11ecc6a167f156
zoo868e <zoo868e@gmail.com>
2021-10-20 15:49:22 +0800

    init

---
+++ gnuplot_learning/README.md
@@ -0,0 +1,1 @@
+g++ -o output gnutest.cpp includetest.cpp -lboost_system -lboost_iostreams -lboost_filesystem
---
+++ gnuplot_learning/includetest.cpp
@@ -0,0 +1,5 @@
+#include"bits/stdc++.h"
+#include"includetest.h"
+void printHello(){
+	std::cout << "Hello World!" << std::endl;
+}
