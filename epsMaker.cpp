52744a134ca89a953bd468be033e5ea5e603647b
zoo868e <zoo868e@gmail.com>
2021-10-20 15:40:03 +0800

    Implement the method that accuracyChecker will use
    void initEPS(Gnuplot&, bool):
    	initialize the container and the second argument is to control
    	use color or not
    void setOutputFile(Gnuplot&, string filename):
    	set the output filename for container
    void setTitle(Gnuplot&, string title):
    	set the title for the following graph
    void plotLines(Gnuplot&, vector<map<double, int>>, vector<double>):
    	print the appearance times of each win tricks and print the edge
    	of each continuous win trick.
    void setCloseFile(Gnuplot&):
    	close the output file.

---
+++ epsMaker.cpp
@@ -0,0 +1,53 @@
+#include"./epsMaker.h"
+void initEPS(Gnuplot& gp, bool useColor){
+	if(useColor)
+		gp << "set term postscript eps enhanced color\n";
+	else
+		gp << "set term postscript eps enhanced\n";
+
+}
+void setOutputFile(Gnuplot& gp, string filename){
+	gp << "set output \"" << filename << "\"\n";
+}
+void setTitle(Gnuplot& gp, string title){
+	gp << "set title \"" << title << "\"\n";
+}
+void plotLines(Gnuplot& gp, vector<map<double, int>> eachScoreAppearTimes, vector<double> gaps){
+	gp << "plot";
+	const int N = eachScoreAppearTimes.size();
+	double height = 0;
+	vector<vector<pair<double, double>>> xy_pts(N);
+	for(int i = 0;i < N;i++){
+		int datasize = 0;
+		for(auto score:eachScoreAppearTimes[i])
+			datasize += score.second;
+		for(auto score:eachScoreAppearTimes[i]){
+			xy_pts[i].push_back(make_pair(score.first, (double)score.second / datasize));
+			height = height > (double)score.second / datasize ? height:(double)score.second / datasize;
+		}
+	}
+	vector<vector<pair<double, double>>> xy_gaps;
+	for(auto x:gaps){
+		xy_gaps.push_back(vector<pair<double, double>>({pair<double, double>(x, 0), pair<double, double>(x, height)}));
+	}
+	for(int i = 0;i < N;i++){
+		gp << gp.file1d(xy_pts[i]) << "with lines title \'" << i + 7 << "\',";
+	}
+	for(auto line:xy_gaps){
+		gp << gp.file1d(line) << "with lines notitle lt rgb \"black\", ";
+	}
+	gp << endl;
+}
+void setCloseFile(Gnuplot& gp){
+	gp << "set output\n";
+}
+void plotSplitLines(Gnuplot& gp, vector<double> gaps){
+	vector<vector<pair<double, double>>> xy_pts;
+	for(auto x:gaps){
+		xy_pts.push_back(vector<pair<double, double>>({pair<double, double>(x, 0), pair<double, double>(x, 1)}));
+	}
+	for(auto line:xy_pts){
+		gp << "plot" << gp.file1d(line) << "with lines, ";
+	}
+	gp << endl;
+}
