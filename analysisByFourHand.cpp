ba6a941764eef05205681a5ed08d42e23e1825d1
zoo868e <zoo868e@gmail.com>
2021-11-03 15:30:29 +0800

    Implement the new experiment
    analysisByFourHand:
    	sh: ./analysisByFourHand DataSetFileName pre_FormulaID
    	descriptions:
    		Read the Data set by the second argument and use third
    		argument to be formula id.
    		1. Load the data set
    		2. Compute the original score of each player in each
    		PartialGame
    		3. Read the new line to get the new score matrix of
    		FixFormula1, print the result to standard output and
    		then read next line.

--- analysisByFourHand.cpp
+++ analysisByFourHand.cpp
@@ -2,6 +2,7 @@
 #include "DataSetReader.h"
 
 using namespace std;
+const vector<double> ArgumentOfFormulaTwo{1.045, 1.229, 1.629, 2.066, 2.912, 3.527, 1.019, 1.2, 0.5106, 0.2381, 0.2422, 2.043, 1.122, 0.01089, 0.3142, 2.107, 1.366};
 
 int main(int argc, char** argv){
 	if(argc < 3){
@@ -10,7 +11,57 @@ int main(int argc, char** argv){
 		return 1;
 	}
 	vector<Game> game;
-	loadGame(argv[1], game);
+	vector<PartialGame> partialgames;
+//	loadGame(argv[1], game);
+	loadPartialGames(argv[1], partialgames);
+	Experiment PartialgameExperiment(partialgames, atoi(argv[2]));
+	PartialgameExperiment.Set_scorematrix(ArgumentOfFormulaTwo);
+	PartialgameExperiment.PartialGamePreScorer();
+	/*
+	for(auto game:PartialgameExperiment.Partialgames){
+		for(auto player:game.player){
+			cout << "player: " << endl;
+			auto hand = player.getPlayer();
+			auto cards = hand.first.getcard();
+			for(auto s:cards){
+				for(auto c:s)cout << c << " ";
+				cout << endl;
+			}
+			cout << player.score << endl;
+			cout << "------------------\n";
+		}
+		cout << "suit = " << game.suit << endl;
+		cout << "DDSwin = " << game.DDSwin << endl;
+		cout << "Maker = " << game.maker << endl << endl;
+	}*/
+	string input;
+	vector<string> scorematrixb;
+	vector<double> scorematrix;
+	while(getline(cin, input)){
+		scorematrixb.clear();
+		scorematrix.clear();
+		scorematrixb = split(input, " ");
+		for(int i = 0;i < (int)scorematrixb.size();i++){
+			scorematrix.push_back(stod(scorematrixb[i]));
+		}
+		int ercode = PartialgameExperiment.Set_FixFormulaArgumentList(scorematrix);
+		if(ercode){
+			cerr << "--Wrong size of scorematrix-- Size = " << ercode << endl;
+		}
+		else{
+			PartialgameExperiment.PartialGameTrainScorer();
+			double ans = getcorr(PartialgameExperiment);
+			cout << ans << endl;
+			if(ans == -2){
+				cout << "Experiment: " << endl;
+				cout << "len(score): " << PartialgameExperiment.score.size() << endl;
+				cout << "score = ";
+				for(auto x:PartialgameExperiment.score)cout << x << " ";
+				cout << endl;
+			}
+		}
+	}
+	/*
 	for(auto g:game){
 		auto gameinfo = g.getGame();
 		auto dds = gameinfo.second;
@@ -32,5 +83,9 @@ int main(int argc, char** argv){
 			cout << "----------------" << endl;
 		}
 	}
+	Experiment FourHand(game, 2);
+	FourHand.Set_scorematrix(ArgumentOfFormulaTwo);
+	FourHand.GameScorer();
+	*/
 	return 0;
 }
