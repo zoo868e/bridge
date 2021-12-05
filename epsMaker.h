48ccea394a27a922e57d966e8379df93cc36e08f
zoo868e <zoo868e@gmail.com>
2021-12-05 16:07:09 +0800

    Add the new function to plot
    void plotLines(..., vector<map<int, int>>, ...)
    plot the figure that the score is int

--- epsMaker.h
+++ epsMaker.h
@@ -5,4 +5,5 @@ void initEPS(Gnuplot& gp, bool useColor);
 void setOutputFile(Gnuplot& gp, string filename);
 void setTitle(Gnuplot& gp, string title);
 void plotLines(Gnuplot& gp, vector<map<double, int>> eachScoreAppearTimes, vector<double> gaps);
+void plotLines(Gnuplot& gp, vector<map<int, int>> eachScoreAppearTimes, vector<double> gaps);
 void setCloseFile(Gnuplot& gp);
