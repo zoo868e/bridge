39e2bc11fa5bce509aa5c17824a0198190d0e20a
zoo868e <zoo868e@gmail.com>
2022-01-26 21:34:23 +0800

    Remove the comment part

--- subprocesstest.cpp
+++ subprocesstest.cpp
@@ -7,33 +7,10 @@ int main(int argc, char* argv[]){
 	string filename = "./data/dataForC";
 	vector<Team> team;
 	loadTeam(filename, team);
-	/*
-	cout << team.size() << endl;
-	for(auto x:team){
-		cout << "Player 1:" << endl;
-		for(auto i:x.player[0].hand.getcard()){
-			for(auto card:i)cout << card << " ";
-			cout << endl;
-		}
-		cout << endl;
-		cout << "Player2:" << endl;
-		for(auto i:x.player[1].hand.getcard()){
-			for(auto card:i)cout << card << " ";
-			cout << endl;
-		}
-		cout << "suit: " << x.suit << endl;
-		cout << "DDS: " << x.DDSwin << endl;
-		cout << "maker: " << x.maker << endl;
-		cout << endl;
-	}*/
+
 	Experiment E = Experiment(team);
 	if(argc > 1)E.setformulaid(stoi(argv[1]));
-	/*
-	for(int i = 0;i < (int)E.score.size();i++){
-		cout << "DDS: " << E.DDS[i] << ", score: " << E.score[i] << endl;
-	}*/
-//	E.scorer();
-//	cout << getcorr(E) << endl;
+
 	string s;
 	vector<string> scorematrixb;
 	vector<double> scorematrix;
@@ -48,23 +25,6 @@ int main(int argc, char* argv[]){
 		if(ercode != 0)cout << "--Wrong size of scorematrix--Size = " << ercode << endl;
 		else{
 			E.scorer();
-/*			for(int i = 0;i < (int)E.score.size();i++){
-				vector<vector<int>> hand;
-				hand = E.teams[i].player[1].hand.getcard();
-				for(auto x:hand){
-					for(auto z:x)cout << z << " ";
-					cout << " | ";
-				}
-				cout << endl;
-				hand = E.teams[i].player[0].hand.getcard();
-				for(auto x:hand){
-					for(auto z:x)cout << z << " ";
-					cout << " | ";
-				}
-				cout << endl;
-				cout << "DDS: " << E.teams[i].DDSwin << ", suit: " << E.teams[i].suit << endl;
-				cout << "score: " << E.score[i] << endl;
-			}*/
 			double ans = getcorr(E);
 			cout << ans << endl;
 			if(ans == -2){
